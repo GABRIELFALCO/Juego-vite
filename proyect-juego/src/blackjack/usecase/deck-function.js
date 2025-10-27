da14d36adb77eefb4543862b065e9971282cd5d0
Gabriel Falcon <famg0405@gmail.com>
2025-10-27 10:20:49 -0600

    fix: sxe corrigio la funcionalidad del metodo deck de forma correcta

--- proyect-juego/src/blackjack/usecase/deck-function.js
+++ proyect-juego/src/blackjack/usecase/deck-function.js
@@ -4,12 +4,12 @@ export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
     let deck = [];
     //ciclo interactivo
     for( let i = 2; i <= 10; i++ ) {
-        for( let tipo of tipos ) {
-            deck.push( i + tiposDeCarta);
+        for( let tipo of tiposDeCarta ) {
+            deck.push( i + tipo);
         }
     }
 
-    for( let tipo of tipos ) {
+    for( let tipo of tiposDeCarta ) {
         for( let esp of tiposEspeciales ) {
             deck.push( esp + tipo);
         }
